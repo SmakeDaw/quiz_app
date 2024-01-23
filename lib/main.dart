04cb1ac136bfebefa5d871a8bc081fbc3394d028
kaixa <kitjipongit@gmail.com>
2024-01-23 23:50:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'quiz_sless.dart';
+
+void main(){
+  runApp(
+    Quiz_Statel(),
+  );
+}
---
+++ lib/quiz_sless.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Quiz_Statel extends StatelessWidget{
+  Quiz_Statel({super.key});
+
+
+
+  @override
+  Widget build(context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepOrange, 
+                        Colors.pinkAccent],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter
+                        ),
+                      ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset('smakedaw/quiz-logo.png',),
+              const Text('Learn Flutter the fun way!', 
+                style: TextStyle(fontSize: 32, color: Colors.white,),
+              ),
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                onPressed:() {
+
+                },
+                child: const Text('Start Quiz'),
+              ),
+              
+              ], 
+              ),
+              
+            ),
+          ),
+          ),
+          );
+  }
+}
