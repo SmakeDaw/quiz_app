b9cae88e5b5e27e5d392ceb509c878a291cdbb73
Pathomporn.R <neunghub.17@gmail.com>
2024-01-24 11:20:37 +0700

    - Fixed Bug

--- lib/quiz_sless.dart
+++ lib/quiz_sless.dart
@@ -1,45 +1,45 @@
 import 'package:flutter/material.dart';
 
-class Quiz_Statel extends StatelessWidget{
-  Quiz_Statel({super.key});
-
-
+class Quiz_Statel extends StatelessWidget {
+  const Quiz_Statel({super.key});
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: [Colors.deepOrange, 
-                        Colors.pinkAccent],
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter
-                        ),
-                      ),
+                colors: [Colors.deepOrange, Colors.pinkAccent],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter),
+          ),
           child: Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Image.asset('/images/quiz-logo.png',),
-              const Text('Learn Flutter the fun way!', 
-                style: TextStyle(fontSize: 32, color: Colors.white,),
-              ),
-              const SizedBox(height: 20,),
-              ElevatedButton(
-                onPressed:() {
-
-                },
-                child: const Text('Start Quiz'),
-              ),
-              
-              ], 
-              ),
-              
+                Image.asset(
+                  'assets/images/quizlogo.png',
+                ),
+                const Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Start Quiz'),
+                ),
+              ],
             ),
           ),
-          ),
-          );
+        ),
+      ),
+    );
   }
 }
